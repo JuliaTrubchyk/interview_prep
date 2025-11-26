348667c108b545185733f9b680d976eb78c33c1c
Yuliia <julia.koshovets@gmail.com>
2025-11-25 17:56:34 -0800

    Added Calculator class and first Unit tests for it

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,15 @@
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        List<String> numbers = List.of("(123) 456-7890", "987-654-3210", "555 123 4567");
-        String telNum = "(123) 456-7890";
-        String cleaned = telNum.replaceAll("[ .]", "");
-        System.out.println(cleanPhoneNumbers(numbers));
+        Calculator calc = new Calculator();
+        int sum = calc.add(4, 5);
+        int diff = calc.subtract(11, 7);
+        System.out.println(sum);
+        System.out.println(diff);
     }
 
-    public static List<String> cleanPhoneNumbers(List<String> numbers){
-        List<String> list = new ArrayList<>();
-        for(String tel : numbers){
-            list.add(tel.replaceAll("\\D+", ""));
-        }
-        return list;
-    }
+
 
 
 
